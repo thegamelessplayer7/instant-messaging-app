a5a180d6077bcb59b7037dd60f952302b2bcf43e
thegamelessplayer7 <thegamelessplayer7@gmail.com>
2023-05-21 23:33:03 -0500

    add basic functionality

--- instant-messaging-app/src/App.js
+++ instant-messaging-app/src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('');
+  const [messages, setMessages] = useState([]);
+
+  
+  console.log(messages);
+
+  const sendMessage = (event) => {
+    event.preventDefault();
+    setMessages([...messages, input]);
+    setInput('');
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>Programming an instant messaging app</h1>
+
+      <form action="submit">
+        <input value={input} onChange={event => setInput(event.target.value)} type="text" />
+        <button type='submit' onClick={sendMessage}>Send Message</button>
+      </form>
+
+    {
+      messages.map((message) => (
+        <p>{message}</p>
+      ))
+    }
+
+     {/* actual messages */}
     </div>
   );
 }
